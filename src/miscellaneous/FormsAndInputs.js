d1993b9e707e7665acc0d6c22f713e35aa7cc175
Shantanu <thegeek.004@gmail.com>
2018-10-19 00:11:59 +0530

    Reusable React Input

--- src/miscellaneous/FormsAndInputs.js
+++ src/miscellaneous/FormsAndInputs.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
 
+function MyTextInput (props) {
+  function handleChange (event) {
+    if (props.onChange) props.onChange(event)
+  }
+  return (
+    <p>
+      <input type='email' value={props.value} name={props.name} ref={props.inputRef} onChange={handleChange} />
+    </p>
+  )
+}
+
 class FormsAndInputs extends Component {
   constructor (props) {
     super(props)
     this.state = {
       fullName: '',
-      content: ''
+      content: '',
+      email: ''
     }
     this.inputFullNameRef = React.createRef()
+    this.inputEmailRef = React.createRef()
   }
 
   handleSubmit = (event) => {
@@ -27,8 +40,7 @@ class FormsAndInputs extends Component {
 
   handleFocusClick = (event) => {
     event.preventDefault()
-    // we don't need the 'current' attribute because the inline ref is for a particular input and not generic
-    this.inputContentRef.focus()
+    this.inputEmailRef.current.focus()
   }
 
   handleClearClick = (event) => {
@@ -44,22 +56,18 @@ class FormsAndInputs extends Component {
 
   render () {
     const { fullName } = this.state
+    const { email } = this.state
     return (
       <div>
         <h1>Forms and Inputs</h1>
         <p>Full name is: {fullName}</p>
         <form onSubmit={this.handleSubmit}>
+          <MyTextInput inputRef={this.inputEmailRef} value={email} name='email' onChange={this.handleInputChange} />
 
-          {/* Creating a ref and defining it in the constructor */}
+          {/* If we don't add 'onChange' method then we won't be able to edit the input field */}
           <p><input ref={this.inputFullNameRef} type='text' placeholder='Your Name' value={fullName} name='fullName' onChange={this.handleInputChange} /></p>
-
-          {/* Creating an inline ref */}
           <p><textarea ref={node => this.inputContentRef = node} placeholder='Your message' name='content' required={true} onChange={this.handleInputChange}></textarea></p>
-
           <p><button>Send Message</button></p>
-
-          {/* The buttons below, go to the onSubmit function after executing their onClick functions.
-          So we need to include event.preventDefault() in those onClick methods */}
           <p><button onClick={this.handleFocusClick}>Focus</button></p>
           <p><button onClick={this.handleClearClick}>Clear</button></p>
         </form>
